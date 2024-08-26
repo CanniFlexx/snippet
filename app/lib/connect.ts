1a51c47ba0785ce05eb17bf842bf58b5968187ed
Anish Samanta <95096002+CanniFlexx@users.noreply.github.com>
2024-08-26 17:51:07 +0530

    Database config updated

--- app/lib/connect.ts
+++ app/lib/connect.ts
@@ -1,13 +1,28 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
-async function connect(): Promise<void> {
-  try {
-    await mongoose.connect(process.env.MONGO_URL as string);
-    console.log("db connected...");
-  } catch (error) {
-    console.error(error);
-  }
+const MONGO_URL = process.env.MONGO_URL!;
+
+interface MongooseConn {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null
 }
 
+let cached: MongooseConn = (global as any).mongoose; 
 
-export default connect;
+if(!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export const connect = async () => {
+  if(cached.conn) {
+    return cached.conn;
+  }
+  cached.promise = mongoose.connect(MONGO_URL, {
+    dbName: "clerk-details",
+    bufferCommands: false,
+    connectTimeoutMS: 30000
+  });
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
